7848c25b80a5083b1e3ad1637393b51358d2fa02
AahilD <aahild07@gmail.com>
2021-08-11 18:52:58 -0600

    Changed some formatting

--- tweetContent.py
+++ tweetContent.py
@@ -2,16 +2,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-
 def getHoroscope():
     #Read URL and enter it into HTML parser
     r = requests.get("https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign=4")
     soup = BeautifulSoup(r.content, 'html.parser')
-
     #Isolate paragraph to tweet
     a = soup.find('p')
-
     #Edit paragraph
     a = a.get_text()
-
     return a
